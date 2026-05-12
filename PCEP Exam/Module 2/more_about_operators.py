23b5a33c4cd5d1a5afd864f72dbdd172d16de08d
RafaelStevanato <rafael.stevanato@outlook.com>
2026-05-12 13:25:24 -0300

    ..

---
+++ PCEP Exam/Module 2/more_about_operators.py
@@ -0,0 +1,6 @@
+print(12)
+print(-2)
+print((2+3) * 2)
+print(0.1)
+print(0.1 +0.1 +0.1) #Float number are not exactly precised
+print(2 **2 **3) #Exponentation are executed from the right to the left
---
+++ PCEP Exam/Module 2/type_casting.py
@@ -0,0 +1,4 @@
+height_cm = float(input("Sua altura em centímetros: "))
+
+print ("Sua altura em pés é: ", height_cm /30.48)
+
